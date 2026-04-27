bf6102f7fc6b2ffbe3dfdc629bb33b346d7299c1
5dishapatil <disha.k.patil5@gmail.com>
2026-04-27 16:30:30 +0530

    Add RewardClaim admin configuration and filters

--- community_app/admin.py
+++ community_app/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from community_app.models import TaskCompletion
+from community_app.models import TaskCompletion, RewardClaim
 
 
 @admin.register(TaskCompletion)
 class TaskCompletionAdmin(admin.ModelAdmin):
     list_display = ("user", "task_id", "points", "location", "completed_at")
-    ordering     = ("-completed_at",)
+    list_filter = ("completed_at",)
+    search_fields = ("user__username", "user__email", "task_id", "task_text", "location")
+    ordering = ("-completed_at",)
+
+
+@admin.register(RewardClaim)
+class RewardClaimAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "reward_name",
+        "voucher_amount",
+        "milestone_points",
+        "voucher_code",
+        "status",
+        "claimed_at",
+    )
+    list_filter = ("milestone_points", "status", "claimed_at")
+    search_fields = ("user__username", "user__email", "reward_name", "voucher_code")
+    ordering = ("-claimed_at",)
